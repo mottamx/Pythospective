848129aad97124fce76c4e87777fe2c290e5000f
mottamx <cmottaa@gmail.com>
2023-05-25 01:18:38 -0600

    Now the ToDo list became file practice
    Todo List now saves in a file.

--- ToDo plaintext/main.py
+++ ToDo plaintext/main.py
@@ -1,31 +1,45 @@
 #Desktop script to create a simple Todo List
+#Currently there are so many repeated lines, will need to fix
+
 import sys
 
-print(sys.version_info) #Must be Python 3.10 for match function.
-
-todos = []
+#print(sys.version_info) #Must be Python 3.10 for match function.
 
 while True:
     user = input("Type add, show, edit, complete or exit: ")
     user = user.strip() #Remove spacing
     match user:
         case 'add':
-            todo=input("Enter a todo: ")
-            todos.append(todo)
+            todo = input("Enter a todo: ") + "\n"
+            with open(r'D:\MScCarlos\Documents\GitHub\Pythospective\ToDo plaintext\todos.txt', 'r') as file:
+                todos = file.readlines()
+                todos.append(todo)
+            with open(r'D:\MScCarlos\Documents\GitHub\Pythospective\ToDo plaintext\todos.txt', 'w') as file:
+                file.writelines(todos)
         case 'show':
+            with open(r'D:\MScCarlos\Documents\GitHub\Pythospective\ToDo plaintext\todos.txt', 'r') as file:
+                todos = file.readlines()
+            #new_todos=[item.strip('\n') for item in todos] #List comprehension   
             for index, item in enumerate(todos):
-                item = item.title()
+                item = item.strip('\n').title()
                 textf=f"{index+1} -> {item}"
                 print(textf)
         case 'edit':
             number = int(input("Number of the todo to edit: "))
             number = number - 1
             new_todo=input("Enter a new todo: ")
-            todos[number]=new_todo
+            with open(r'D:\MScCarlos\Documents\GitHub\Pythospective\ToDo plaintext\todos.txt', 'r') as file:
+                todos = file.readlines()
+            todos[number]=new_todo + '\n'
+            with open(r'D:\MScCarlos\Documents\GitHub\Pythospective\ToDo plaintext\todos.txt', 'w') as file:
+                file.writelines(todos)
         case 'complete':
             number = int(input("Number of the todo to complete: "))
-            number = number - 1
-            todos.pop(number)
+            with open(r'D:\MScCarlos\Documents\GitHub\Pythospective\ToDo plaintext\todos.txt', 'r') as file:
+                todos = file.readlines()
+            todos.pop(number - 1)
+            with open(r'D:\MScCarlos\Documents\GitHub\Pythospective\ToDo plaintext\todos.txt', 'w') as file:
+                file.writelines(todos)
         case 'exit':
             break
         case _:
