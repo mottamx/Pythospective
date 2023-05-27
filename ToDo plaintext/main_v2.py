b9f71924ea4035b0dc218993fdc22ec4ee037197
mottamx <cmottaa@gmail.com>
2023-05-27 17:27:01 -0600

    Fixes, added thy except
    try except in edit and complete cases.
    Fixed startswith in the matching cases

--- ToDo plaintext/main_v2.py
+++ ToDo plaintext/main_v2.py
@@ -10,14 +10,14 @@ while True:
     user = input("Type add, show, edit, complete or exit: ")
     user = user.strip() #Remove spacing
     
-    if 'add' in user or 'new' in user:
+    if user.startswith('add'):
         todo = user[4:] #List slicing
         with open(r'D:\MScCarlos\Documents\GitHub\Pythospective\ToDo plaintext\todos.txt', 'r') as file:
             todos = file.readlines()
-            todos.append(todo)
+            todos.append(todo+'\n')
         with open(r'D:\MScCarlos\Documents\GitHub\Pythospective\ToDo plaintext\todos.txt', 'w') as file:
             file.writelines(todos)
-    elif 'show' in user:
+    elif user.startswith('show'):
         with open(r'D:\MScCarlos\Documents\GitHub\Pythospective\ToDo plaintext\todos.txt', 'r') as file:
             todos = file.readlines()
         #new_todos=[item.strip('\n') for item in todos] #List comprehension   
@@ -25,25 +25,32 @@ while True:
             item = item.strip('\n').title()
             textf=f"{index+1} -> {item}"
             print(textf)
-    elif 'edit' in user:
-        text = user[5:].strip()
-        print(text)
-        number = int(text)
-        new_todo=input("Enter a new todo: ")
-        with open(r'D:\MScCarlos\Documents\GitHub\Pythospective\ToDo plaintext\todos.txt', 'r') as file:
-            todos = file.readlines()
-        todos[number-1]=new_todo + '\n'
-        with open(r'D:\MScCarlos\Documents\GitHub\Pythospective\ToDo plaintext\todos.txt', 'w') as file:
-            file.writelines(todos)
-    elif 'complete' in user:
-        text = user[9:].strip()
-        number = int(text)
-        with open(r'D:\MScCarlos\Documents\GitHub\Pythospective\ToDo plaintext\todos.txt', 'r') as file:
-            todos = file.readlines()
-        todos.pop(number - 1)
-        with open(r'D:\MScCarlos\Documents\GitHub\Pythospective\ToDo plaintext\todos.txt', 'w') as file:
-            file.writelines(todos)
-    elif 'exit' in user:
+    elif user.startswith('edit'):
+        try:
+            text = user[5:].strip()
+            number = int(text)
+            new_todo=input("Enter a new todo: ")
+            with open(r'D:\MScCarlos\Documents\GitHub\Pythospective\ToDo plaintext\todos.txt', 'r') as file:
+                todos = file.readlines()
+            todos[number-1]=new_todo + '\n'
+            with open(r'D:\MScCarlos\Documents\GitHub\Pythospective\ToDo plaintext\todos.txt', 'w') as file:
+                file.writelines(todos)
+        except ValueError:
+            print("Your command is not valid, edit expects a number")
+            continue
+    elif user.startswith('complete'):
+        try:
+            text = user[9:].strip()
+            number = int(text)
+            with open(r'D:\MScCarlos\Documents\GitHub\Pythospective\ToDo plaintext\todos.txt', 'r') as file:
+                todos = file.readlines()
+            todos.pop(number - 1)
+            with open(r'D:\MScCarlos\Documents\GitHub\Pythospective\ToDo plaintext\todos.txt', 'w') as file:
+                file.writelines(todos)
+        except IndexError:
+            print("The number is out of range")
+            continue            
+    elif user.startswith('exit'):
         break
     else:
         print("Unknown command")
